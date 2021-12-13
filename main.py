4b10c0393e80c2c9c397c7e2926f5fb69183cfea
KaraszekG <95226417+KaraszekG@users.noreply.github.com>
2021-12-13 13:03:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+#Írj programot, mely beolvas két egész számot, és kiírja a képernyőre a nagyobbikat!
+
+
+egy = int(input("Írj egy számot: "))
+ketto = int(input("Írj mégegy számot: "))
+
+if egy < ketto:
+  print("A(z) " , ketto , " nagyobb")
+
+else:
+  print("A(z) " , egy , " nagyobb")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
